7fbfad539230a2efc16aebaa011776045e6e8202
Julian Carrico <Julian.carrico@gmail.com>
2026-02-04 14:38:20 -0500

    second commit of the traversy crud mvc tutorial

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
+
+//load confg
 dotenv.config({ path: './config/config.env' })
 
 connectDB()
+
+//Determine level of logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+//added .engine after exphbs
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs'
+})
+)
+app.set('view engine', '.hbs')
+
+//Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 8000
 
